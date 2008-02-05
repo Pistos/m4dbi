6635658268e9b6cc06fc0614dc9360cb88152a6b
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-02-05 05:47:02 +0000

    Fixed specs (bad bacon syntax). Added DBI::DatabaseHandle#select_column. Added DBI::Row#method_missing to provide Struct-like access to fields.

--- spec/core.rb
+++ spec/core.rb
@@ -9,39 +9,39 @@ describe 'select_column' do
     name = $dbh.select_column(
       "SELECT name FROM authors LIMIT 1"
     )
-    name.should.be 'author1'
+    name.should.equal 'author1'
   end
   
   it 'should select one column of first row' do
     name = $dbh.select_column(
       "SELECT name FROM authors ORDER BY name DESC"
     )
-    name.should.be 'author2'
+    name.should.equal 'author2'
   end
   
   it 'should select first column of first row' do
     name = $dbh.select_column(
       "SELECT name, id FROM authors ORDER BY name DESC"
     )
-    name.should.be 'author2'
+    name.should.equal 'author2'
   end
   
 end
 
-describe 'Struct behaviour' do
+describe 'accessors' do
   
   it 'should provide access via ".fieldname" syntax' do
     row = $dbh.select_one(
       "SELECT * FROM posts ORDER BY author_id DESC LIMIT 1"
     )
-    row.should.not.be nil
+    row.should.not.equal nil
     
     row._id.should.be.same_as row[ 'id' ]
     row.id_.should.be.same_as row[ 'id' ]
     row.author_id.should.be.same_as row[ 'author_id' ]
-    row.text.should.be.same_as row[ text ]
+    row.text.should.be.same_as row[ 'text' ]
     
-    row.text.should.be 'Second post.'
+    row.text.should.equal 'Second post.'
   end
   
 end
