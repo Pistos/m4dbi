372c414bb669790de28e7fd687cb5bee64ed8d32
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-02-05 04:44:42 +0000

    Added empty lib files. Added empty specs. Added spec helper. Added Rakefile.

---
+++ spec/core.rb
@@ -0,0 +1,8 @@
+require 'spec/helper'
+require 'm4dbi'
+
+describe 'foo' do
+  before do
+  end
+
+end
---
+++ spec/helper.rb
@@ -0,0 +1,8 @@
+$LOAD_PATH.unshift(
+  File.expand_path(
+    File.join(
+      File.dirname( __FILE__ ),
+      "../lib"
+    )
+  )
+)
