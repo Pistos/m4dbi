88239d56eb877c4e125281d273b6bd28229e352b
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-02-06 19:36:57 +0000

    Better implementation using meta_def.

--- lib/m4dbi/model.rb
+++ lib/m4dbi/model.rb
@@ -3,46 +3,39 @@ require 'metaid'
 
 module DBI
   class Model
-    def self.dbh
-      from = self
-      while !(d = from.instance_variable_get("@dbh")) && from != Object
-        from = from.superclass
-      end
-      d
-    end
-    
-    def self.[]( pk_value )
-      ancestral_trait[ :dbh ].select_one(
-        "SELECT * FROM #{ancestral_trait[ :table ]} WHERE #{pk} = ?",
-        pk_value
-      )
-    end
-    
-    def self.pk
-      @pk || 'id'
+    def self.dbh=( handle )
+      @dbh = handle
     end
     def self.pk=( col )
       @pk = col
     end
-    
     def self.table=( t )
       @table = t
+    end
+    
+    def self.[]( pk_value )
+      @dbh.select_one(
+        "SELECT * FROM #{@table} WHERE #{@pk} = ?",
+        pk_value
+      )
     end
     
   end
   
   def self.Model( table, pk = 'id' )
-    c = Class.new( DBI::Model )
-    
-    h = DBI::DatabaseHandle.last_handle
-    if h.nil?
-      raise DBI::Error.new( "Attempted to create a Model class without first connecting to a database." )
+    Class.new( DBI::Model ) do
+      h = DBI::DatabaseHandle.last_handle
+      if h.nil?
+        raise DBI::Error.new( "Attempted to create a Model class without first connecting to a database." )
+      end
+      
+      meta_def( :inherited ) do |c|
+        c.dbh = h
+        c.table = table
+        c.pk = pk
+      end
+      
     end
-    
-    c.trait[ :dbh ] = h
-    c.trait[ :table ] = table
-    c.pk = pk
-    c
   end
   
 end
