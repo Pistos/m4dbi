438587fd7e9944da438c01177de5554116d525e4
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-02-06 18:46:23 +0000

    Next draft, trying to get Model working on a basic level. Thanks to Kashia for helping with @dbh in Model.

--- lib/m4dbi/database-handle.rb
+++ lib/m4dbi/database-handle.rb
@@ -2,11 +2,32 @@ require 'dbi'
 
 module DBI
   class DatabaseHandle
+    alias old_initialize initialize
+    def initialize( handle )
+      DBI::DatabaseHandle.last_handle = handle
+      old_initialize( handle )
+    end
+    
     def select_column( statement, *bindvars )
       row = select_one( statement, *bindvars )
       if row
         row[ 0 ]
       end
     end
+    
+    class << self
+      def last_handle
+        @handle# ||= create_handle
+      end
+      
+      def last_handle=( handle )
+        @handle = handle
+      end
+    end
+    
   end
 end
+
+
+
+
--- lib/m4dbi/model.rb
+++ lib/m4dbi/model.rb
@@ -1,7 +1,36 @@
 require 'dbi'
+require 'metaid'
 
 module DBI
   class Model
+    def self.dbh
+      from = self
+      while !(d = from.instance_variable_get("@dbh")) && from != Object
+        from = from.superclass
+      end
+      d
+    end
+    
+    def self.[]( pk_value )
+      dbh.select_one( "SELECT * FROM #{@table} WHERE #{@pk} = ?", pk_value )
+    end
+    
+    def self.pk=( col )
+      @pk = col
+    end
+    def self.table=( t )
+      @table = t
+    end
     
   end
+  
+  def self.Model( table )
+    c = Class.new( DBI::Model )
+    h = c.instance_variable_set( '@dbh', DBI::DatabaseHandle.last_handle )
+    if h.nil?
+      raise DBI::Error.new( "Attempted to create a Model class without first connecting to a database." )
+    end
+    c
+  end
+  
 end
