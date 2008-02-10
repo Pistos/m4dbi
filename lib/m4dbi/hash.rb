9ae3729945ec030adf8f679d9ba3bb8b7f224c49
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-02-10 07:07:34 +0000

    Adjusted Hash#to_where_clause and specs.

--- lib/m4dbi/hash.rb
+++ lib/m4dbi/hash.rb
@@ -1,7 +1,14 @@
 class Hash
+  
   def to_where_clause
-    keys.map { |field|
+    # The clause items and the values have to be in the same order.
+    keys_ = keys
+    clause = keys_.map { |field|
       "#{field} = ?"
     }.join( " AND " )
+    values_ = keys_.map { |key|
+      self[ key ]
+    }
+    [ clause, values_ ]
   end
 end
--- spec/hash.rb
+++ spec/hash.rb
@@ -1,18 +1,21 @@
 require 'spec/helper'
 
 describe 'Hash' do
-  it 'should be convertible to a where subclause' do
+  it 'should be convertible to a where subclause and matching value Array' do
     h = {
       :a => 2,
       :b => 'foo',
       :abc => Time.now,
       :xyz => 9.02,
     }
-    c = h.to_where_clause
+    clause, values = h.to_where_clause
+    
     s = "a = ? AND b = ? AND abc = ? AND xyz = ?"
-    c.length.should.equal s.length
-    h.keys.each do |key|
-      c.should.match /#{key} = ?/
+    clause.length.should.equal s.length
+    h.each do |key,value|
+      clause.should.match /#{key} = ?/
+      values.find { |v| v == value }.should.not.be.nil
     end
+    values.size.should.equal h.keys.size
   end
 end
