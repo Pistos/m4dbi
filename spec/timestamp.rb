44b6acb54641dfc874aa96432e59a834dbb48269
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-04-17 12:58:28 +0000

    Added DBI::Timestamp specs.

---
+++ spec/timestamp.rb
@@ -0,0 +1,18 @@
+require 'spec/helper'
+
+$dbh = DBI.connect( "DBI:Pg:m4dbi", "m4dbi", "m4dbi" )
+# See test-schema.sql and test-data.sql
+
+describe 'a DBI::Timestamp object' do
+  before do
+    @ts = $dbh.sc "SELECT ts FROM many_col_table LIMIT 1"
+  end
+  it 'acts like a Time object' do
+    should.raise( NoMethodError ) do
+      @ts.no_such_method_on_time_objects
+    end
+    should.not.raise( NoMethodError ) do
+      @ts.strftime "%H:%M"
+    end
+  end
+end
