eb4db15a8617940125030f7bd3432b8a93de2f71
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-08-23 01:29:28 -0400

    Now using relative paths for subrequires.

--- lib/m4dbi.rb
+++ lib/m4dbi.rb
@@ -2,11 +2,13 @@ require 'rubygems'
 
 M4DBI_VERSION = '0.6.0'
 
-require 'm4dbi/traits'
-require 'm4dbi/hash'
-require 'm4dbi/array'
-require 'm4dbi/database-handle'
-require 'm4dbi/row'
-require 'm4dbi/timestamp'
-require 'm4dbi/model'
-require 'm4dbi/collection'
+__DIR__ = File.expand_path( File.dirname( __FILE__ ) )
+
+require "#{__DIR__}/m4dbi/traits"
+require "#{__DIR__}/m4dbi/hash"
+require "#{__DIR__}/m4dbi/array"
+require "#{__DIR__}/m4dbi/database-handle"
+require "#{__DIR__}/m4dbi/row"
+require "#{__DIR__}/m4dbi/timestamp"
+require "#{__DIR__}/m4dbi/model"
+require "#{__DIR__}/m4dbi/collection"
