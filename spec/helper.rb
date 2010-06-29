2e166ba62651e5b3220e58d3982ec04e2b110986
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-06-28 20:46:26 -0400

    Now using RDBI in spec helper.

--- spec/helper.rb
+++ spec/helper.rb
@@ -12,21 +12,25 @@ $LOAD_PATH.unshift(
 
 require 'm4dbi'
 
-puts "DBI version: #{DBI::VERSION}"
+# puts "RDBI version: #{RDBI::VERSION}"
 puts "M4DBI version: #{M4DBI_VERSION}"
 
 # See test-schema*.sql and test-data.sql
 def connect_to_spec_database( database = ( ENV[ 'M4DBI_DATABASE' ] || 'm4dbi' ) )
-  driver = ENV[ 'M4DBI_DRIVER' ] || "DBI:Pg"
-  puts "Using DBI driver: '#{driver}'"
-  DBI.connect( "#{driver}:#{database}", "m4dbi", "m4dbi" )
+  driver = ENV[ 'M4DBI_DRIVER' ] || "PostgreSQL"
+  puts "Using RDBI driver: '#{driver}'"
+  case driver
+  when 'PostgreSQL'
+    require 'rdbi/driver/postgresql'
+  end
+  M4DBI.connect( driver, database: 'm4dbi', username: 'm4dbi' )
 end
 
 def reset_data( dbh = $dbh, datafile = "test-data.sql" )
   dir = File.dirname( __FILE__ )
   File.read( "#{dir}/#{datafile}" ).split( /;/ ).each do |command|
     if not command.strip.empty?
-      dbh.do command
+      dbh.execute command
     end
   end
 end
