4c08e86fda074a050137c660237674e1d322fc86
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-07-03 15:43:56 -0400

    Use specified database name instead of hard-coded "m4dbi".

--- spec/helper.rb
+++ spec/helper.rb
@@ -23,7 +23,7 @@ def connect_to_spec_database( database = ( ENV[ 'M4DBI_DATABASE' ] || 'm4dbi' ) 
   when 'PostgreSQL'
     require 'rdbi/driver/postgresql'
   end
-  M4DBI.connect( driver, database: 'm4dbi', username: 'm4dbi' )
+  M4DBI.connect( driver, database: database, username: 'm4dbi' )
 end
 
 def reset_data( dbh = $dbh, datafile = "test-data.sql" )
